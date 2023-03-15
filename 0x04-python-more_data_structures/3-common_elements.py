08e6731140a9dec19da82d441634ca3aa124d626
Anjames123 <ajibadejames19@gmail.com>
2023-03-15 12:17:51 -0700

    tasks

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    return (set_1 & set_2)
-
-=================================
-4-only_diff_elements.py
-
-#!/usr/bin/python3
-def only_diff_elements(set_1, set_2):
-    return (set_1 ^ set_2)
+    return (set_1 & set_2))
