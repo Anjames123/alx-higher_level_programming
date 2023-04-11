8300543633e4689871f7a3cd61862381c06ac792
Anjames123 <ajibadejames19@gmail.com>
2023-04-11 06:56:32 -0700

    tasks

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+    """
+    Returns a sorted list of all the attributes and methods of an object.
+
+    Parameters:
+    obj (object): The object to inspect.
+
+    Returns:
+    A list of strings representing the names of all the attributes and methods of the object, sorted in alphabetical order.
+
+    """
+    return sorted(dir(obj))
