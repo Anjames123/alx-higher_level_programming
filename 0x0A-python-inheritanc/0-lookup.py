cf0393d3c0012e08eca2dedb8d60673ed9e2ef53
Anjames123 <ajibadejames19@gmail.com>
2023-04-11 06:28:40 -0700

    tasks

--- 0x0A-python-inheritanc/0-lookup.py
+++ 0x0A-python-inheritanc/0-lookup.py
@@ -4,11 +4,4 @@ def lookup(obj):
     """
     Returns the list of available attributes and methods of an object.
     """
-    attributes = []
-    methods = []
-    for attr in dir(obj):
-        if callable(getattr(obj, attr)):
-            methods.append(attr)
-        else:
-            attributes.append(attr)
-            return sorted(attributes) + sorted(methods)
+    return sorted(dir(obj))
