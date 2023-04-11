42b7597d18a6890d88ea10d635e4a12d2ca54223
Anjames123 <ajibadejames19@gmail.com>
2023-04-11 07:06:32 -0700

    lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
+""" Define an attribute lookup function """
+
 
 def lookup(obj):
     """
     Returns a sorted list of all the attributes and methods of an object.
-
-    Parameters:
-    obj (object): The object to inspect.
-
-    Returns:
-    A list of strings representing the names of all the attributes and methods of the object, sorted in alphabetical order.
-
     """
-    return sorted(dir(obj))
+    return (dir(obj))
