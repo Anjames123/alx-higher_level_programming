54a91e294df17e9e14052f9db1d0524d1ab1a615
Anjames123 <ajibadejames19@gmail.com>
2023-04-11 08:52:53 -0700

    tasks

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+class MyList(list):
+    """
+    A class MyList that inherits from the built-in list class in Python.
+    """
+
+    def print_sorted(self):
+        """
+        Public instance method that prints the list in ascending order.
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
