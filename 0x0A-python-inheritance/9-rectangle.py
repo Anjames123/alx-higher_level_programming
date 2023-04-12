4e4fd0c11efffb32ac5a77f9b7c7eb99286c1038
Anjames123 <ajibadejames19@gmail.com>
2023-04-11 17:35:20 -0700

    tasks

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -11,7 +11,8 @@ class BaseGeometry:
 
     def area(self):
         """
-        Public instance method that raises an Exception with the message area() is not implemented
+        Public instance method that raises an Exception with the message
+        area() is not implemented
         """
         raise Exception("area() is not implemented")
 
@@ -19,8 +20,10 @@ class BaseGeometry:
         """
         Public instance method that validates value:
         * you can assume name is always a string
-        * if value is not an integer: raise a TypeError exception, with the message <name> must be an integer
-        * if value is less or equal to 0: raise a ValueError exception with the message <name> must be greater than 0
+        * if value is not an integer: raise a TypeError exception, with the
+        message <name> must be an integer
+        * if value is less or equal to 0: raise a ValueError exception with
+        the message <name> must be greater than 0
         """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
@@ -49,6 +52,7 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """
-        Print and return the following rectangle description: [Rectangle] <width>/<height>
+        Print and return the following rectangle description:
+        Rectangle] <width>/<height>
         """
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
