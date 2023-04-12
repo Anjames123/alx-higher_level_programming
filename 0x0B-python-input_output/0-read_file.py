dd54968632a31ddcb58a5b4a44f7a8f57186da59
Anjames123 <ajibadejames19@gmail.com>
2023-04-12 15:41:17 -0700

    tassks

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,14 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""module to read a text file"""
+
 
 def read_file(filename=""):
     """
     This function reads a text file (UTF-8) and prints its content to stdout.
-
-    Args:
-        filename (str): The path to the text file. Defaults to an empty string.
-
-    Returns:
-        None
     """
     with open(filename, encoding="utf-8") as f:
         content = f.read()
