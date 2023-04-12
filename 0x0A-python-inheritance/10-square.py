d32367c3b4f4dc1a2582674c419a0ede7be15b9a
Anjames123 <ajibadejames19@gmail.com>
2023-04-11 18:06:44 -0700

    tasks

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -11,8 +11,8 @@ class BaseGeometry:
 
     def area(self):
         """
-        Public instance method that raises an Exception with
-        the message area() is not implemented
+        Public instance method that raises an Exception with the message
+        area() is not implemented
         """
         raise Exception("area() is not implemented")
 
@@ -21,9 +21,9 @@ class BaseGeometry:
         Public instance method that validates value:
         * you can assume name is always a string
         * if value is not an integer: raise a TypeError exception,
-        with the message <name> must be an integer
+          with the message <name> must be an integer
         * if value is less or equal to 0: raise a ValueError exception
-        with the message <name> must be greater than 0
+          with the message <name> must be greater than 0
         """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
