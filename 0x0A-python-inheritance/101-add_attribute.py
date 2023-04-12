096fc4c2e3e3379bbce79948da03b0d0a3299519
Anjames123 <ajibadejames19@gmail.com>
2023-04-11 18:26:38 -0700

    tasks

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Adds a new attribute to an object if it's possible """
+
+
+def add_attribute(obj, attr, value):
+    """
+    Adds a new attribute to an object if it's possible.
+
+    Args:
+        obj: The object to add the attribute to.
+        attr: The name of the attribute to add.
+        value: The value to assign to the attribute.
+
+    Raises:
+        TypeError: If the object can't have new attributes.
+    """
+    if not isinstance(obj, object):
+        raise TypeError("can't add new attribute")
+    setattr(obj, attr, value)
