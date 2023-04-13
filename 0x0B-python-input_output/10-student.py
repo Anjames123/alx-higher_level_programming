71b8d460c33e57697314447b6b4129b5dc2be004
Anjames123 <ajibadejames19@gmail.com>
2023-04-12 17:06:50 -0700

    tassks

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -6,13 +6,15 @@ class Student:
     """Represent a student"""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student instance with the given name and age"""
+        """Initialize a new Student instance
+        with the given name and age"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Return a dictionary representation of this Student instance"""
+        """Return a dictionary representation
+        of this Student instance"""
         if attrs is None:
             return self.__dict__
         return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
