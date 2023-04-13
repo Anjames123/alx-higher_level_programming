850f770ef2bcadff4c9c8b60fc687115f2ec5ddf
Anjames123 <ajibadejames19@gmail.com>
2023-04-12 17:55:40 -0700

    tasks

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,40 +1,44 @@
 #!/usr/bin/python3
-"""Reads stdin line by line and computes metrics."""
+
+"""
+Reads stdin line by line and computes metrics.
+
+Input format: <IP> - [<date>] "GET /projects/260 HTTP/1.1" <status> <size>
+Every 10 lines or on keyboard interrupt, prints:
+- Total file size: <total size>
+- Number of lines by status code in ascending order (if seen):
+  - <status>: <number>
+"""
 
 import sys
 
-metrics = {
-    "size": 0,
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
+# Initialize variables for tracking metrics
+total_file_size = 0
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+line_count = 0
 
 try:
-    for i, line in enumerate(sys.stdin, start=1):
-        split_line = line.split()
-        try:
-            status_code = split_line[-2]
-            if status_code in metrics:
-                metrics[status_code] += 1
-            metrics["size"] += int(split_line[-1])
-        except Exception:
-            pass
+    # Read input from stdin line by line
+    for line in sys.stdin:
+        line_count += 1
+        fields = line.split(" ")
+        status_code = int(fields[3])
+        file_size = int(fields[4])
 
-        if i % 10 == 0:
-            print("File size: {}".format(metrics["size"]))
-            for k, v in sorted(metrics.items()):
-                if k != "size" and v != 0:
-                    print("{}: {}".format(k, v))
+        # Update metrics
+        total_file_size += file_size
+        status_codes[status_code] += 1
+
+        # Every 10 lines or on keyboard interrupt, print metrics
+        if line_count % 10 == 0:
+            print("Total file size:", total_file_size)
+            for code in sorted(status_codes.keys()):
+                if status_codes[code] > 0:
+                    print(str(code) + ":", status_codes[code])
 
 except KeyboardInterrupt:
-    print("File size: {}".format(metrics["size"]))
-    for k, v in sorted(metrics.items()):
-        if k != "size" and v != 0:
-            print("{}: {}".format(k, v))
-    sys.exit(0)
+    # Print final metrics on keyboard interrupt
+    print("Total file size:", total_file_size)
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print(str(code) + ":", status_codes[code])
