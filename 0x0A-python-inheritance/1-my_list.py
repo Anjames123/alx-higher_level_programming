6368701306419e151fe2135c4ad6dcba6507a0d6
Anjames123 <ajibadejames19@gmail.com>
2023-04-14 08:06:42 -0700

    task updated

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,6 +9,12 @@ class MyList(list):
     A class MyList that inherits from list
     """
 
+    def __str__(self):
+        """
+        Return a string representation of the list.
+        """
+        return str(list(self))
+
     def print_sorted(self):
         """
         Public instance method that prints the list, but sorted
